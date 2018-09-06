748a15dd60409ada4bf034e317e08ff28fbe9c6d
Valentin Piret <valentin.piret@affinitic.be>
2018-09-06 13:16:48 +0200

    Add configlet anomalies_reporting

---
+++ src/rescuearea/core/browser/controlpanel.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from plone.app.registry.browser import controlpanel
+from rescuearea.core import _
+from plone.autoform import directives as form
+from zope import schema
+from zope.interface import Interface
+
+
+class IAnomaliesReportingSettingsSchema(Interface):
+
+    form.widget('anomalies_reporting', klass='pat-tinymce')
+    anomalies_reporting = schema.Text(
+        title=_(u'Anomalies reporting')
+    )
+
+
+class AnomaliesReportingSettingsEditForm(controlpanel.RegistryEditForm):
+
+    schema = IAnomaliesReportingSettingsSchema
+    label = _(u'Anomalies reporting settings')
+    description = _(u'')
+
+    def updateFields(self):
+        super(AnomaliesReportingSettingsEditForm, self).updateFields()
+
+    def updateWidgets(self):
+        super(AnomaliesReportingSettingsEditForm, self).updateWidgets()
+
+
+class AnomaliesReportingSettingsControlPanel(controlpanel.ControlPanelFormWrapper):
+    form = AnomaliesReportingSettingsEditForm
