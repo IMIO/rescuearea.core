e68034a498a4553e50163646ba8300daa85cc363
Valentin Piret <valentin.piret@affinitic.be>
2018-09-10 14:33:58 +0200

    Add popup content type

---
+++ src/rescuearea/core/content/popup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+from plone.app.textfield import RichText
+from plone.dexterity.content import Container
+from plone.supermodel import model
+from zope import schema
+from zope.interface import implements
+from zope.interface import Invalid
+from zope.interface import invariant
+
+from rescuearea.core import _
+
+
+class IPopUp(model.Schema):
+
+    titre = schema.TextLine(
+        title=_(u'Titre'),
+        required=False,
+    )
+
+    description = RichText(
+        title=_(u'Description'),
+        default_mime_type='text/html',
+        required=False,
+    )
+
+    start = schema.Date(
+        title=_(u'Date start'),
+        required=False,
+    )
+
+    end = schema.Date(
+        title=_(u'Date end'),
+        required=False,
+    )
+
+    @invariant
+    def validate_start_end(data):
+        if data.start is not None and data.end is not None:
+            if data.start > data.end:
+                raise Invalid(_(u"The start date must be before the end date."))
+
+
+class PopUp(Container):
+    implements(IPopUp)
