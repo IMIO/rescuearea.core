53d0e635cf291873aeb8ba266ba76c590b18a19c
Martin Peeters <martin.peeters@affinitic.be>
2018-09-16 23:15:46 +0200

    Fix render of tooltips for boolean widgets

--- src/rescuearea/core/interfaces.py
+++ src/rescuearea/core/interfaces.py
@@ -2,7 +2,12 @@
 """Module where all interfaces, events and exceptions live."""
 
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+from plone.app.z3cform.interfaces import IPloneFormLayer
 
 
 class IRescueareaCoreLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
+
+
+class IRescueAreaFormLayer(IPloneFormLayer):
+    """Request interface installed via browserlayer.xml"""
