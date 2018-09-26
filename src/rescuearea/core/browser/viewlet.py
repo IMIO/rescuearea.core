720aada6a03972a2d202ad6060735e42a813c4f0
Valentin Piret <valentin.piret@affinitic.be>
2018-09-26 11:47:28 +0200

    Upgrade popup style

--- src/rescuearea/core/browser/viewlet.py
+++ src/rescuearea/core/browser/viewlet.py
@@ -4,6 +4,8 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.viewlets import ViewletBase
 from plone import api
 
+from rescuearea.core import _
+
 import datetime
 
 
@@ -24,6 +26,13 @@ class PopUpViewlet(ViewletBase):
             start = popup.start
             end = popup.end
             if start <= today <= end:
-                return popup.getObject().richtext_desc.output
+                text = _(u'<p>From {0} to {1}').format(
+                    start.strftime("%d-%m-%Y"),
+                    end.strftime("%d-%m-%Y")
+                )
+                return '{0} {1}'.format(
+                    text,
+                    popup.getObject().richtext_desc.output
+                )
 
         return False
