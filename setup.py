5831367b13b144e34b3d8a90bb7a52b6b4388560
Laurent Lasudry <lasudry@gmail.com>
2019-02-01 17:03:39 +0100

    Back to development: 1.0b6

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='rescuearea.core',
-    version='1.0b5',
+    version='1.0b6.dev0',
     description="An add-on for Plone",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
