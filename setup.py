a30750a0f1dbc2e0cdf69211699d0ed593c6f88d
Piret Valentin <valentin.piret@affinitic.be>
2019-02-25 13:53:14 +0100

    Back to development: 1.0b9

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='rescuearea.core',
-    version='1.0b8',
+    version='1.0b9.dev0',
     description="An add-on for Plone",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
